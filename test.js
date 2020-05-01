2766ce3c14f31220eccc1d45b31e7391cdbf5360
Andrey <brandrews@yandex.ru>
2020-05-01 10:48:32 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,47 @@
+'use strict'
+
+let a = 5;
+console.log(a);
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(leftBorderWidth);
+
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+
+// let persone = {
+//     name: "Andrew",
+//     age: 30,
+//     isMarried: false
+// };
+
+// console.log(persone.name);
+
+
+// var pop = ["raz", "dva", "tri"];
+
+// console.log(pop[1]);
+
+// alert("Hello,World!");
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer = prompt("Есть ли вам 18?", "Да");
+// console.log(typeof(pop));
+
+// console.log("pop" + "- object");
+
+
+let des = 10,
+odin = 10;
+
+console.log(des++);
+console.log(odin--);
